bc88b598cea7c6951c1f62c95ce7794baae27fbb
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-09 16:35:30 -0400

    fix: correcciones uso de comillas

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,25 +1,21 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
 function auth(req, res, next) {
-    const cookie = req.cookie.authorization;
+  const cookie = req.cookie.authorization
 
-    if (!cookie) {
-        throw new AppError(HTTP_STATUS.unauthorized, "La cookie es necesaria");
-    }
+  if (!cookie) {
+    throw new AppError(HTTP_STATUS.unauthorized, 'La cookie es necesaria')
+  }
 
-    try {
-        const payload = jwt.verify(
-            cookie,
-            process.env.JWT_SECRET
-        );
+  try {
+    const payload = jwt.verify(cookie, process.env.JWT_SECRET)
 
-        req.user = payload;
+    req.user = payload
 
-        next();
-    } catch {
-        throw new AppError(HTTP_STATUS.unauthorized, "Token inválido");
-    }
-
+    next()
+  } catch {
+    throw new AppError(HTTP_STATUS.unauthorized, 'Token inválido')
+  }
 }
 
-module.exports = "auth";
+module.exports = 'auth'
