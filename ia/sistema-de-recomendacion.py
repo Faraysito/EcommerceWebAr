9a9c72033c475dbb2ad1d866357044d8503b1545
Tiare Salazar <tiar.salazar@duocuc.cl>
2026-06-13 22:33:08 -0400

    fix: agregar función de recursos similares

--- ia/sistema-de-recomendacion.py
+++ ia/sistema-de-recomendacion.py
@@ -6,16 +6,19 @@ from difflib import SequenceMatcher
 from sqlalchemy import create_engine
 
 # la direccion actual no funciona, el servidor no se ejecuta
-engine = create_engine("postgresql://postgres:NorthumbriaBernard1998.@db.vjseeipqkgqgdtxxkcdv.supabase.co:5432/postgres")
+engine = create_engine("postgresql://postgres.lwkikmipbbrhgypyqcqy:[YOUR-PASSWORD]]@aws-1-us-east-2.pooler.supabase.com:5432/postgres")
 
 """ 
 Recibe parámetros de producto y cantidad de elementos a devolver
 """
-def recursos_similares(producto, k):
+def recursos_similares(id, k):
     # data frame
     dc_listing = pd.read_sql("SELECT id, name, category_id, price FROM product", engine)
 
     temp_df = dc_listing.copy()
+
+    # producto seleccionado
+    producto = pd.read_sql("SELECT id, name, category_id, price FROM product WHERE id = %(id)s", engine, params={"id": id}).iloc[0]
 
     """
     Calcula la similitud entre el producto seleccionado y los elementos utilizando columnas
@@ -24,7 +27,7 @@ def recursos_similares(producto, k):
     # similitud precios (40% de peso)
     temp_df['price_similarity'] = temp_df['price'].apply(lambda x: similitud_precio(x, producto.price) *0.4)
     # similitud nombres (40% de peso)
-    temp_df['name_similarity'] = temp_df['name'].apply(lambda x: SequenceMatcher(None, x.lower(), producto.name.lower()).ratio() *0.4)
+    temp_df['name_similarity'] = temp_df['name'].apply(lambda x: SequenceMatcher(None, producto["name"].lower(), x.lower()).ratio() *0.4)
     # similitud categoria (20% de peso)
     temp_df['category_similarity'] = temp_df['category_id'].apply(lambda x: (1 if x == producto.category_id else 0) *0.2)
 
@@ -40,4 +43,4 @@ def similitud_precio(p1, p2):
     diferencia = abs(p1 - p2)
     return 1 / (1 + (diferencia / 100)**2)
 
-print(recursos_similares(producto, 8))
+print(recursos_similares("0ec1ca7c-6a7a-4d99-b2b6-b11052e521ba", 8))
